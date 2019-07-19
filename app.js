3832eb3de46298d71f354f0316d49b91e42d103f
VeselAbaya <Vandervise465@gmail.com>
2019-07-19 22:39:19 +0300

    deploy

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // CORS
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', process.env.CLIENT_URL);
+  res.header('Access-Control-Allow-Origin', process.env.CORS_DOMEN);
   res.header('Access-Control-Allow-Methods', 'GET, DELETE');
   next();
 });
